e329e2f870e4f1993cdde7bcaca517506c52a439
WwWilsonLin <847799751@qq.com>
2018-01-16 23:54:42 +0800

    add some icon,add nav bar with no css

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!-- <img src="./assets/logo.png"> -->
+    <div id="navbar">
+      <div class="nav">
+        <div class="nav-icon"></div>
+        <div class="nav-body">搜索</div>
+      </div>
+      <div class="nav">
+        <div class="nav-icon"></div>
+        <div class="nav-body">我喜欢的音乐</div>
+      </div>
+      <div class="nav">
+        <div class="nav-icon"></div>
+        <div class="nav-body">我创建的歌单</div>
+      </div>
+    </div>
     <router-view/>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.prototype.$axios = axios;
+
+axios.defaults.withCredentials= true;
 
 /* eslint-disable no-new */
 new Vue({
