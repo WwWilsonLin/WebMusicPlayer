e7c833060e5b95112282850731874272a41f4638
WwWilsonLin <847799751@qq.com>
2018-02-08 17:54:07 +0800

    add search file(unfinished)

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,24 @@
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
     <div id="navbar">
-      <div class="nav">
-        <div class="nav-icon"></div>
-        <div class="nav-body">搜索</div>
-      </div>
-      <div class="nav">
-        <div class="nav-icon"></div>
-        <div class="nav-body">我喜欢的音乐</div>
-      </div>
-      <div class="nav">
-        <div class="nav-icon"></div>
-        <div class="nav-body">我创建的歌单</div>
-      </div>
+      <!-- <router-link to="/find"> -->
+        <div class="nav">
+          <div class="nav-icon"></div>
+          <div class="nav-body">搜索</div>
+        </div>
+      <!-- </router-link> -->
+      <!-- <router-link to="/likebill"> -->
+        <div class="nav">
+          <div class="nav-icon"></div>
+          <div class="nav-body">我喜欢的音乐</div>
+        </div>
+      <!-- </router-link> -->
+      <!-- <router-link to="/bill"> -->
+        <div class="nav">
+          <div class="nav-icon"></div>
+          <div class="nav-body">我创建的歌单</div>
+        </div>
+      <!-- </router-link> -->
     </div>
     <router-view/>
   </div>
@@ -26,12 +32,34 @@ export default {
 </script>
 
 <style>
+*{
+  margin:0 0;
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
+  font-family: 微软雅黑;
+}
+body{
+  background-color: rgb(249,249,249);
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+}
+#navbar{
+  width: 180px;
+  height: 90%;
+  background-color: rgb(244,245,245);
+  border: 2px solid rgb(211,211,211);
+}
+.nav{
+  margin: 20px 8px;
+}
+a{
+  text-decoration: none;
+}
+.nav-body{
+  font-size: 18px; 
 }
 </style>
---
+++ src/components/search.vue
@@ -0,0 +1,26 @@
+<template>
+<div id="search-body">
+	<input type="input" id="input" placeholder="搜索音乐、歌手">
+</div>
+</template>
+
+<script>
+	
+</script>
+
+<style scoped>
+#search-body{
+	width: 80%;
+	height: 91%;
+	left: 184px;
+	top: 0;
+	position: absolute;
+}
+#input{
+	background-color: #FFF;
+	width: 60%;
+	margin: 24px 10%;
+	padding: 8px;
+	font-size: 18px;
+}
+</style>
